694a22257cbbb790dac12b7d207448b89ed80ae7
widmann <widmann@uni-leipzig.de>
2021-10-14 13:37:11 +0200

    eye_rejblinkeyelink.m initial import

---
+++ eye_rejblinkeyelink.m
@@ -0,0 +1,146 @@
+% eye_rejblinkeyelink() - Mark Eyelink detected blinks for
+%                         rejection/interpolation
+%
+% Usage:
+%   >> [ EYE ] = eye_rejblinkeyelink( EYE, 'key1', value1, 'key2', value2, 
+%                                     'keyn', valuen );
+%
+%  Inputs:
+%   EYE       - EEGLAB EEG structure
+%   'chans'   - vector channel indices to process
+%
+% Outputs:
+%   EYE       - EEGLAB EEG structure
+%
+% Note:
+%   Find Eyelink blink and saccade events, do some basic consistency
+%   checks, find saccades enclosing the blink events and mark blinks in
+%   EYE.reject.rejmanualE substructure for later rejection or
+%   interpolation.
+%
+% Author: Andreas Widmann, 2021
+%
+% See also:
+%   eye_rejsigloss, eye_interp
+
+% Copyright (C) 2021 Andreas Widmann, University of Leipzig, widmann@uni-leipzig.de
+%
+% This program is free software; you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation; either version 2 of the License, or
+% (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with this program; if not, write to the Free Software
+% Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+function [ EYE ] = eye_rejblinkeyelink( EYE, varargin )
+
+if nargin < 3
+    error( 'Not enough input arguments.' )
+end
+
+Arg = struct( varargin{ : } );
+
+if ~isfield( Arg, 'chans' ) || isempty( Arg.chans )
+    error( 'chans input argument is required.' )
+end
+
+if isempty( EYE.reject.rejmanualE )
+    EYE.reject.rejmanualE = zeros( size( EYE.data ) );
+end
+
+% Detect saccades
+startSaccIdx = find( strcmp( 'STARTSACC', { EYE.event.type } ) );
+endSaccIdx = find( strcmp( 'ENDSACC', { EYE.event.type } ) );
+
+[ Sacc( 1:length( startSaccIdx ) ).start ] = EYE.event( startSaccIdx ).latency;
+[ Sacc.eye ] = EYE.event( startSaccIdx ).eye;
+
+for iEye = unique( [ Sacc.eye ] )
+
+    if sum( [ Sacc.eye ] == iEye ) ~= sum( [ EYE.event( endSaccIdx ).eye ] == iEye )
+%         for iTmp = unique( [ Sacc.eye ] )
+%             fprintf( 'Eye %d saccade start latencies:', iTmp );
+%             [ Sacc( [ Sacc.eye ] == iTmp ).start ]
+%             fprintf( 'Eye %d saccade end latencies:', iTmp );
+%             [ EYE.event( endSaccIdx( [ EYE.event( endSaccIdx ).eye ] == iTmp ) ).latency ]
+%         end
+        error( 'Number of STARTSACC events (%d) does not match number of ENDSACC events (%d) for eye %d.', sum( [ Sacc.eye ] == iEye ), length( endSaccIdx ), iEye )
+    end
+
+    [ Sacc( [ Sacc.eye ] == iEye ).end ] = EYE.event( endSaccIdx( [ EYE.event( endSaccIdx ).eye ] == iEye ) ).latency;
+
+end
+
+if any( [ Sacc.start ] > [ Sacc.end ] )
+    error( 'Saccade end latency < start latency.' )
+end
+
+% Detect blinks
+startBlinkIdx = find( strcmp( 'STARTBLINK ', { EYE.event.type } ) );
+endBlinkIdx = find( strcmp( 'ENDBLINK', { EYE.event.type } ) );
+
+[ Blink( 1:length( startBlinkIdx ) ).start ] = EYE.event( startBlinkIdx ).latency;
+[ Blink.eye ] = EYE.event( startBlinkIdx ).eye;
+
+for iEye = unique( [ Blink.eye ] )
+
+    if sum( [ Blink.eye ] == iEye ) ~= sum( [ EYE.event( endBlinkIdx ).eye ] == iEye )
+%         for iTmp = unique( [ Blink.eye ] )
+%             [ Sacc( [ Sacc.eye ] == iTmp ).start; Sacc( [ Sacc.eye ] == iTmp ).end ]
+%             fprintf( 'Eye %d blink start latencies:', iTmp );
+%             [ Blink( [ Blink.eye ] == iTmp ).start ]
+%             fprintf( 'Eye %d blink end latencies:', iTmp );
+%             [ EYE.event( find( strcmp( 'ENDBLINK', { EYE.event.type } ) & [ EYE.event.eye ] == iTmp ) ).latency ]
+%         end
+        error( 'Number of STARTBLINK events (%d) does not match number of ENDBLINK events (%d) for eye %d. %d', sum( [ Blink.eye ] == iEye ), length( endBlinkIdx ), iEye )
+    end
+
+    [ Blink( [ Blink.eye ] == iEye ).end ] = EYE.event( endBlinkIdx( [ EYE.event( endBlinkIdx ).eye ] == iEye ) ).latency;
+
+end
+
+if any( [ Blink.start ] > [ Blink.end ] )
+    error( 'Blink end latency < start latency.' )
+end
+
+for iBlink = 1:length( Blink )
+
+    saccIdx = find( [ Sacc.start ] < Blink( iBlink ).start & [ Sacc.eye ] == Blink( iBlink ).eye, 1, 'last' );
+
+    if isempty( saccIdx )
+        saccIdx = find( [ Sacc.start ] <= Blink( iBlink ).start & [ Sacc.eye ] == Blink( iBlink ).eye, 1, 'last' );
+        warning( 'STARTBLINK latency equals STARTSACC latency.' )
+    end
+
+    if isempty( saccIdx ) % Ongoing blink at file start; cut until end of first saccade
+%         saccIdx = find( [ sacc.end ] > blink( iBlink ).end & [ sacc.eye ] == blink( iBlink ).eye, 1, 'first' );
+%         EYE.reject.rejmanualE( Arg.chans( blink( iBlink ).eye ), 1:sacc( saccIdx ).end ) = 1;
+        Blink( iBlink )
+        [ Sacc( [ Sacc.eye ] == Blink( iBlink ).eye ).start ]
+        error( 'No saccade with STARTSACC <= STARTBLINK latency found.' )
+    end
+
+    if Sacc( saccIdx ).end < Blink( iBlink ).end
+        Blink( iBlink )
+        Sacc( saccIdx )
+        error( 'ENDSACC < ENDBLINK latency.' )
+    end
+
+    Blink( iBlink ).STARTSACC = Sacc( saccIdx ).start;
+    Blink( iBlink ).ENDSACC = Sacc( saccIdx ).end;
+    EYE.reject.rejmanualE( Arg.chans( Blink( iBlink ).eye ), Blink( iBlink ).STARTSACC:Blink( iBlink ).ENDSACC ) = 1;
+
+end
+
+EYE.etc.Sacc = Sacc;
+EYE.etc.Blink = Blink;
+
+end
+
